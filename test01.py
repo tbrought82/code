5c148555b87705490a6867b0bc9fe9c38e6c1c99
Tad Broughton <tad.broughton@hpinc.com>
2020-05-04 14:04:28 -0400

    lab 7 update

---
+++ test01.py
@@ -0,0 +1,11 @@
+#!/user/bin/env python3
+
+user_name = input("What is the user name? ")
+day_week = input("What is the day of the week? ")
+
+print("Hello,"+user_name +"!"" Happy "+day_week+"!")
+
+# F STRING
+
+print(f"Hello, {user_name}! Happy {day_week}!")
+
